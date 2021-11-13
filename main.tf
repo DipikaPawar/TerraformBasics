565942c37b617f448986cea4f8d75b7f778cec8a
Dipika Pawar <dipika.m.pawar@gmail.com>
2021-11-13 13:08:11 +0000

    Terraform syntax

--- main.tf
+++ main.tf
@@ -13,23 +13,23 @@ provider "azurerm" {
   features {}
 
 # Create a resource group
-resource "azurerm_resource_group" "myrg" {
-  name     = "dips-myrg"
-  location = "UK South"
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
 }
 
 # Create a virtual network in the learn-resources resource group
-resource "azurerm_virtual_network" "myvnet" {
-  name                = "dips-myvnet"
-  resource_group_name = azurerm_resource_group.myrg.name
-  location            = azurerm_resource_group.myrg.location
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
   address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
 # Create a subnet within the vnet 
-resource "azurerm_subnet" "mysubnet" {
-  name                = "dips-mysubnet"
-  resource_group_name = azurerm_resource_group.myrg.name
-  virtual_network_name = azurerm_virtual_network.myvnet.name
-  address_prefixes    = ["10.0.1.0/24"]
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
 } 
